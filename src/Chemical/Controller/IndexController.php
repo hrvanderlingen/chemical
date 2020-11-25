fcefbacc7c11e22ec2e12c828e38a4f90e1eaa69
rik <hrvanderlingen@gmail.com>
2020-11-25 22:14:45 +0100

    tidy

--- src/Chemical/Controller/IndexController.php
+++ src/Chemical/Controller/IndexController.php
@@ -17,30 +17,5 @@ class IndexController extends AbstractActionController
         $this->config = $config;      
     }
 
-    public function indexAction()
-    {        
-
-        
-        $sid = $this->config['twilio']['account_sid'];
-        $token = $this->config['twilio']['auth_token'];
-        ;
-        $twilio = new Client($sid, $token);
-
-        $new_key = $twilio->newKeys
-                        ->create();
-     
-        echo $this->config['authy']['api_key'];
-        $authy_api = new \Authy\AuthyApi($this->config['authy']['api_key']);
-        $user = $authy_api->registerUser('new_user@email.com', '405-342-5699', 57); 
-
-        if($user->ok()) {
-            
-        } else {
-            foreach($user->errors() as $field => $message) {
-                printf("$field = $message\n");
-            }
-        }
-       
-        return new ViewModel();
-    }
+   
 }
